6638d48bc1c1234281aaa62f5e46f7f4e173458f
luizllipe <luisecaduandrade@gmail.com>
2025-02-18 20:21:18 -0300

    63.IF

--- src/fundamentos/DesafioCalculadora.java
+++ src/fundamentos/DesafioCalculadora.java
@@ -7,7 +7,7 @@ public static void main(String[] args) {
     
     System.out.println("=================================================");
     System.out.println("================= CALCULADORA ===================");
-    System.out.println("===================================================\n");
+    System.out.println("==================================================\n");
 
     System.out.print("Digite um numero: ");
     double num1 = entrada.nextDouble();
