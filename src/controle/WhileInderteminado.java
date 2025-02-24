33c359d897e553caf62f3ce4f172ed554c681432
luizllipe <luisecaduandrade@gmail.com>
2025-02-24 19:35:57 -0300

    72. WHILE #2

---
+++ src/controle/WhileInderteminado.java
@@ -0,0 +1,17 @@
+package controle;
+import  java.util.Scanner;
+public class WhileInderteminado {
+	public static void main(String[] args) {
+		
+		Scanner entrada = new Scanner(System.in);
+		
+		 String valor = "";
+		
+		 while(!valor.equalsIgnoreCase("sair")) {
+			 System.out.print("Você diz: ");
+			 valor = entrada.nextLine();	
+		 }
+		 
+		 entrada.close();
+	}
+}
