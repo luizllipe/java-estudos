f1fd6ca747db6abc05aced28372412dc18773852
luizllipe <luisecaduandrade@gmail.com>
2025-02-28 16:46:51 -0300

    79 Desafio FOR - Resposta

---
+++ src/controle/DesafioFOR.java
@@ -0,0 +1,11 @@
+package controle;
+
+public class DesafioFOR {
+	public static void main(String[] args) {
+
+		// Resolução sem um valor númerico
+		for(String v = "#"; !v.equals("######"); v += "#") {
+			System.out.println(v);
+		}
+	}
+}
