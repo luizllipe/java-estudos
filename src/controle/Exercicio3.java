eb5b8622e94cc7e081eee1db0cbd369ad9ec31e8
luizllipe <luisecaduandrade@gmail.com>
2025-03-10 18:49:08 -0300

    Exercicio3

---
+++ src/controle/Exercicio3.java
@@ -0,0 +1,31 @@
+package controle;
+
+import java.util.Scanner;
+
+public class Exercicio3 {
+	public static void main(String[] args) {
+		Scanner entrada = new Scanner(System.in);
+
+		System.out.print("Digite a nota do aluno: ");
+		double nota = entrada.nextDouble();
+		
+		
+		if(nota >= 7) {
+			System.out.println("Aprovado!");
+		} else if(nota >= 5) {
+			System.out.println("Recuperação!");
+		}else {
+			System.out.println("Reprovado!");
+		}
+		
+		
+		
+		
+		
+		
+		
+	entrada.close();	
+
+			
+}
+}
