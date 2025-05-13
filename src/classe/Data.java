68fa61c1b63c4d29015fbd483e8d78b2739ba4ff
luizllipe <luisecaduandrade@gmail.com>
2025-05-13 19:55:24 -0300

    Valor Nulo

--- src/classe/Data.java
+++ src/classe/Data.java
@@ -6,19 +6,21 @@ public class Data {
 	int ano; 
 
 	Data(){
-		dia = 1;
-		mes = 1;
-		ano = 1970;
+		//dia = 1;
+		//mes = 1;
+		//ano = 1970;
+		this(1, 1, 1970);
 	}
 	
-	Data(int dia, int meInicial, int ano){
+	Data(int dia, int mes, int ano){
 		this.dia = dia;
 		this.mes = mes;
 		this.ano = ano;
 	}
 	
 	String obterDataFormatada() {
-		return String.format("%d/%d/%d", dia, mes, ano);
+		final String formato = "%d/%d/%d"; 
+		return String.format(formato, this.dia, mes, ano);
 	}
 	           
 	void imprimirDataFormatada(){
