d2c0b04c044dea44e26747906e2fa1ba24f19e00
luizllipe <luisecaduandrade@gmail.com>
2025-12-06 20:47:45 -0300

    #158.Desafio Herança #01

---
+++ src/oo/heranca/desafio/CarroTeste.java
@@ -0,0 +1,32 @@
+package oo.heranca.desafio;
+
+public class CarroTeste {
+	
+	public static void main(String[] args) {
+		
+		Carro c1 = new Gol();
+		
+		c1.acelerar();
+		System.out.println(c1);
+		
+		c1.acelerar();
+		System.out.println(c1);
+		
+		c1.acelerar();
+		System.out.println(c1);
+		
+		Ferrari c2 = new Ferrari();
+		
+		c2.acelerar();
+		c2.frear();
+		System.out.println(c2);
+		
+		c2.acelerar();
+		c2.frear();
+		System.out.println(c2);
+		
+		c2.acelerar();
+		System.out.println(c2);
+	}
+}
+
