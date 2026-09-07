e1c83d6ee23287d4938da5499b024abe65184022
luizllipe <luisecaduandrade@gmail.com>
2026-09-07 15:23:28 -0300

    #183.Consumer

--- src/lambdas/Produto.java
+++ src/lambdas/Produto.java
@@ -11,4 +11,9 @@ public class Produto  extends Object {
 		this.preco = preco;
 		this.desconto = desconto;
 	}
+	public String toString() {
+		double precoFinal = preco * (1 - desconto);
+		return "Nome: " + nome + " tem preço de R$" + precoFinal;
+				
+	}
 }
